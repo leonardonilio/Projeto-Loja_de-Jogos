7464701e5329df46a3bb3715ab8df8e90025c310
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-02-27 23:53:07 -0300

    Adicionando modelos da branch ModelsCriados

---
+++ src/test/java/com/fatecgru/Projeto/Loja/de/Brinquedos/ProjetoLojaDeBrinquedosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecgru.Projeto.Loja.de.Brinquedos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaDeBrinquedosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
