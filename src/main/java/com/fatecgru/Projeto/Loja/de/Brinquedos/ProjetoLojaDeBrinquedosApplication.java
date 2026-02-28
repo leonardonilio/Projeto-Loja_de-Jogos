928473f3aa38a9a65d22f35daa04ca6f47529414
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-02-27 23:53:07 -0300

    Adicionando modelos da branch ModelsCriados

---
+++ src/main/java/com/fatecgru/Projeto/Loja/de/Brinquedos/ProjetoLojaDeBrinquedosApplication.java
@@ -0,0 +1,13 @@
+package com.fatecgru.Projeto.Loja.de.Brinquedos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojaDeBrinquedosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLojaDeBrinquedosApplication.class, args);
+	}
+
+}
