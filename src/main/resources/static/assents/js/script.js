492c0d043d35c75ff1bb4ce0589c1cb98ad32097
Leonardo Nilio dos Santos <leonardoniliodossantos08@gmail.com>
2026-03-06 16:11:37 -0300

    feat(banner): adição do carrosel de imagem no banner com um codigo que fica trocando a imagem a cada 5 minutos

--- src/main/resources/static/assents/js/script.js
+++ src/main/resources/static/assents/js/script.js
@@ -4,3 +4,66 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 }
+
+/*Script para o carrosel do banner*/ 
+$(document).ready(function(){
+
+let current = 0
+const slides = $(".slides")
+const total = $(".slide").length
+const dots = $(".dot")
+
+function updateSlider(){
+
+slides.css("transform","translateX(-"+current*100+"%)")
+
+dots.removeClass("active")
+dots.eq(current).addClass("active")
+
+}
+
+function nextSlide(){
+
+current++
+
+if(current >= total){
+current = 0
+}
+
+updateSlider()
+
+}
+
+function prevSlide(){
+
+current--
+
+if(current < 0){
+current = total - 1
+}
+
+updateSlider()
+
+}
+
+$(".arrow.right").click(function(){
+nextSlide()
+})
+
+$(".arrow.left").click(function(){
+prevSlide()
+})
+
+dots.click(function(){
+
+current = $(this).index()
+
+updateSlider()
+
+})
+
+setInterval(function(){
+nextSlide()
+},5000)
+
+})
