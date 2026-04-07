d9c2902ca0ac7de0f0b2d749d84cfd5fa3934ed2
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-04-07 19:43:26 -0300

    feat: criando o web controller da página index

---
+++ src/main/java/br/com/umbrellaGames/controller/web/IndexWebController.java
@@ -0,0 +1,34 @@
+package br.com.umbrellaGames.controller.web;
+
+import br.com.umbrellaGames.model.entity.Jogo;
+import br.com.umbrellaGames.service.CategoriaService;
+import br.com.umbrellaGames.service.JogoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/index")
+public class IndexWebController {
+
+    @Autowired
+    private JogoService jogoService;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    // Método que vai listar os jogos no index
+    @GetMapping
+    public String findAll(Model model) {
+        List<Jogo> jogos = jogoService.findAll();
+        model.addAttribute("recomendados", jogoService.findAll());
+        model.addAttribute("gratuitos", jogoService.buscarGratuitos());
+        model.addAttribute("promocao", jogoService.buscarPromocao());
+        model.addAttribute("categorias", categoriaService.findAll());
+        return "index";
+    }
+}
