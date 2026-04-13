a4cab4792e9103db0acb9adec8c98dbcf18ca099
Leonardo Nilio dos Santos <leonardoniliodossantos08@gmail.com>
2026-04-13 20:08:27 -0300

    chore: arquivos deletados

--- java/br/com/umbrellaGames/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.umbrellaGames.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.umbrellaGames.model.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
