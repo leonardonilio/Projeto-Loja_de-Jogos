553027c4fc6d347c10f5eea31c785435c2026354
Leonardo Nilio dos Santos <leonardoniliodossantos08@gmail.com>
2026-04-13 09:15:50 -0300

    feat(infojogos conection): Fiz a conexão da tela info jogos com o backend

--- src/main/java/br/com/umbrellaGames/repository/JogoRepository.java
+++ src/main/java/br/com/umbrellaGames/repository/JogoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.umbrellaGames.model.entity.Jogo;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JogoRepository extends JpaRepository<Jogo, Integer>{
 
